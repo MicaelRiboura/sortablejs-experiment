a7a154de7bc954823926e1b572bd74e5bc2a1b31
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-08 18:21:07 -0300

    feat: add to test different events in sortable list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import Sortable from "../node_modules/sortablejs/modular/sortable.esm.js";
 
 // Simple list
-Sortable.create(simpleList, { /* options */ });
+Sortable.create(simpleList, {
+    // Element is chosen 
+    onChoose: function (evt) {
+        console.log('escolhido');
+	},
+    // Element is unchosen
+    onUnchoose: function(evt) {
+        console.log('náo escolhido');
+	},
+    // Element dragging started
+	onStart: function (evt) {
+        console.log('começado');
+	},
+
+	// Element dragging ended
+	onEnd: function (evt) {
+        console.log('terminado');
+	},
+
+	// Element is dropped into the list from another list
+	onAdd: function (evt) {
+        console.log('adicionados');
+	},
+
+	// Changed sorting within list
+	onUpdate: function (evt) {
+        console.log('atualizado');
+	},
+
+	// Called by any change to the list (add / update / remove)
+	onSort: function (evt) {
+        console.log('sort');
+	},
+ });
+ 
 Sortable.create(simpleList2, { /* options */ });
 Sortable.create(simpleList3, { /* options */ });
