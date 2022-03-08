c858e7b4cec2346a03be30be2fbc2ec6af9b44e1
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-08 17:57:46 -0300

    feat:  add nested list

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import Sortable from "./node_modules/sortablejs/modular/sortable.esm.js";
 
 // Simple list
 Sortable.create(simpleList, { /* options */ });
+Sortable.create(simpleList2, { /* options */ });
+Sortable.create(simpleList3, { /* options */ });
