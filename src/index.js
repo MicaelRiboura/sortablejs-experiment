7852c190fed9a7b2be8ce077c86236a34ed746b6
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-09 15:43:01 -0300

    fix: change scope of update method in sortablejs

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import Sortable from "../node_modules/sortablejs/modular/sortable.esm.js";
 
 // Simple list
 const sortable = Sortable.create(simpleList, {
-	onUpdate: function (event) {
-        console.log('atualizado -> index: ', event.newIndex + 1);
-		const title = event.item.querySelector('.section-title');
-        console.log('element: ', title.innerText);
-		title.innerText = String(title.innerText).replace(/[0-9]{1,}\./, '');
-		title.innerText = `${event.newIndex + 1}. ${title.innerText}`;
+	onUpdate: function (_event) {
+		updateList();
 	},
  });
 
