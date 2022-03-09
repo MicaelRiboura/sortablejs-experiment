7770a88892dc1cb2b7e6f5a8c3a4fb471367b31a
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-09 15:26:42 -0300

    feat: handle item with onUpdate method of sortable

--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
 import Sortable from "../node_modules/sortablejs/modular/sortable.esm.js";
 
 // Simple list
-Sortable.create(simpleList, {
-    // Element is chosen 
-    onChoose: function (evt) {
-        console.log('escolhido');
-	},
-    // Element is unchosen
-    onUnchoose: function(evt) {
-        console.log('náo escolhido');
-	},
-    // Element dragging started
-	onStart: function (evt) {
-        console.log('começado');
-	},
-
-	// Element dragging ended
-	onEnd: function (evt) {
-        console.log('terminado');
-	},
-
-	// Element is dropped into the list from another list
-	onAdd: function (evt) {
-        console.log('adicionados');
-	},
-
-	// Changed sorting within list
-	onUpdate: function (evt) {
-        console.log('atualizado');
-	},
-
-	// Called by any change to the list (add / update / remove)
-	onSort: function (evt) {
-        console.log('sort');
+const sortable = Sortable.create(simpleList, {
+	onUpdate: function (event) {
+        console.log('atualizado -> index: ', event.newIndex + 1);
+		const title = event.item.querySelector('.section-title');
+        console.log('element: ', title.innerText);
+		title.innerText = String(title.innerText).replace(/[0-9]{1,}\./, '');
+		title.innerText = `${event.newIndex + 1}. ${title.innerText}`;
 	},
  });
+
+
+
+
+
+
+
  
 Sortable.create(simpleList2, { /* options */ });
 Sortable.create(simpleList3, { /* options */ });
