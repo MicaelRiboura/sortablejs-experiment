4f944918fdfd1a81da99c9600e4c86a7c689cdbf
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-09 17:28:59 -0300

    feat: add support to dynamic data to include in html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import Sortable from "../node_modules/sortablejs/modular/sortable.esm.js";
+import { listItems } from "./modules/dynamic-list.js";
+// import { teste } from "./modules/dynamic-list.js";
+
+const list = [
+	'Introdução',
+	'Fundamentos',
+	'Primeiro Projeto',
+	'Segundo Projeto',
+	'Intermediário',
+	'Intermediário 2',
+	'Intermediário 3',
+	'Terceiro Projeto',
+	'Quarto Projeto',
+	'Avançado 1',
+	'Avançado 2',
+	'Avançado 3',
+	'Avançado 4',
+	'Avançado 5',
+	'Quinto Projeto',
+	'Sexto Projeto',
+	'Expert 1',
+	'Expert 2',
+	'Expert 3',
+	'Projeto Final',
+];
+
+document.getElementById("app").appendChild(listItems(list));
+
+
+console.log(listItems());
 
 // Simple list
 const sortable = Sortable.create(simpleList, {
@@ -24,6 +54,7 @@ const updateList = () => {
 updateList();
 
 
+
  
-Sortable.create(simpleList2, { /* options */ });
-Sortable.create(simpleList3, { /* options */ });
+// Sortable.create(simpleList2, { /* options */ });
+// Sortable.create(simpleList3, { /* options */ });
